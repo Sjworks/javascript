2a12c66c3dd815991a40455c1a9d57497901c2e9
Son, Sang Jin <sangjin147@naver.com>
2012-02-01 20:57:07 +0900

    자잘한 수정

--- day10/vending_machine.jindo/console.js
+++ day10/vending_machine.jindo/console.js
@@ -16,23 +16,3 @@ var Console = $Class({
 		this._woElement.$value().scrollTop = this._woPage.height();
 	}
 });
-
-
-//이벤트 관련 함수(IE 호환)
-function addEvent(Element, EventType, Handler) {
-
-	if(document.addEventListener) {
-		//DOM 표준
-		Element.addEventListener(EventType, Handler, false);
-	}else if(document.attachEvent) {
-		//IE	
-		Element.attachEvent("on"+EventType, Handler);
-	}
-};
-
-//Array Remove - By John Resig (MIT Licensed)
-Array.prototype.remove = function(from, to) {
-  var rest = this.slice((to || from) + 1 || this.length);
-  this.length = from < 0 ? this.length + from : from;
-  return this.push.apply(this, rest);
-};
